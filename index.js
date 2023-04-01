921d158152493b3e2c2842e1d26ad2c146cd021e
BRIAN BETT <+>
2023-04-01 08:50:53 +0300

    Done

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 function isPalindrome(word) {
+  return false
+  console.log(isPalindrome('word'));//false
+
   // Write your algorithm here
+function isPalindrome(word ='abba'){
+    return true,
+  console.log(isPalindrome('abba'));//true)
 }
+function isPalindrome(word = 'racecar'){
+    return true,
+    console.log(isPalindrome(word = 'racecar')); // true
+      }
+function isPalindrome(word ='a') {
+    return true,
+    console.log(isPalindrome('a'));//true
+  }
+function isPalindrome(word='robot') {
+    return false;
+    console.log(isPalindrome('robot'));//false
+  }
+function isPalindrome(word='ab') {
+    return false
+    console.log(isPalindrome('ab'));//false
+  }
+  }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+/* Add your pseudocode here*/
+console.log(isPalindrome(word
+  ));//false
+  
+  //Add written explanation of your solution here
+//Compared the reversed string to the original and if the two strings are equal it returns 'true' indicating that the input string is isPalindrome,otherwise it returms 'false'
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -22,4 +44,4 @@ if (require.main === module) {
   console.log("=>", isPalindrome("robot"));
 }
 
-module.exports = isPalindrome;
+module.exports = isPalindrome
